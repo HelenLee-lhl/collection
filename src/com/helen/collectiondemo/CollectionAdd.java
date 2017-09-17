4a0480c26954ba1f6e67c927cafce0802fdefafc
HelenLee <helenlee_lhl@163.com>
2017-09-18 01:04:16 +0800

    修改collectionadd示例

--- src/com/helen/collectiondemo/CollectionAdd.java
+++ src/com/helen/collectiondemo/CollectionAdd.java
@@ -11,7 +11,7 @@ public class CollectionAdd {
     public static void main(String [] args){
         List<String> list = new ArrayList<>();
         long timeMillis = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++){
+        for (int i = 0; i < 10000000; i++){
             list.add("字符串" + i);
         }
         System.out.println("Arryalist 用时为：" + (System.currentTimeMillis() - timeMillis));
@@ -19,7 +19,7 @@ public class CollectionAdd {
 
         List<String> list1 = new LinkedList<String>();
         timeMillis = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++){
+        for (int i = 0; i < 10000000; i++){
             list1.add("字符串" + i);
         }
         System.out.println("LinkedList 用时为：" + (System.currentTimeMillis() - timeMillis));
